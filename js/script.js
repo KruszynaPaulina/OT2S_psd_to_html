9e0d35d03ec50a5d69962db2113ac68a9cfc52fb
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2018-05-29 23:39:21 +0200

    Add rwd for header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+var navMenu = $('.navigation__menu');
+var nav = $('.navigation__nav');
+var navList = $('.nav__list');
+var isClick = "unclick";
+var navLogo = $('.navigation__logo');
+var header = $('.header');
 
+
+navMenu.on('click', function(event) {
+    if (isClick === "unclick") {
+    	isClick = "click";
+    	nav.css({"display": "block"});
+    	header.addClass('click');
+    } else {
+    	isClick = "unclick";
+    	nav.css({"display": "none"});
+    	header.removeClass('click');
+    }
+
+    return isClick;
+
+});
