aaec76fcfca6aed903415bb4ffdb7b776a23c8c7
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2018-06-06 12:23:50 +0200

    Add RWD for contact

--- js/script.js
+++ js/script.js
@@ -20,29 +20,6 @@ navMenu.on('click', function(event) {
 
 });
 
-/* Services animation */
-
-var servicesLink = $('.services__link');
-var servicesCells = $('.services__cells');
-var servicesCellTwo = $('.services__cell--two');
-var servicesCellThree = $('.services__cell--three');
-var servicesCellfFour = $('.services__cell--four');
-
-servicesLink.on('mousemove', function(event) {
-	setBackgroundImage()
-    $(this).parent().parent().siblings().css("background-image", "none");
-});
-
-servicesLink.on('mouseleave', function(event) {
-	setBackgroundImage();
-});
-
-function setBackgroundImage() {
-	servicesCellThree.css("background-image", "url(../images/serv_three.jpg)");
-	servicesCellTwo.css("background-image", "url(../images/serv_two.jpg)");
-	servicesCellfFour.css("background-image", "url(../images/serv_four.jpg)");
-};
-
 /* Smooth scrolling */
 
 $('.side__link').click(function() {
